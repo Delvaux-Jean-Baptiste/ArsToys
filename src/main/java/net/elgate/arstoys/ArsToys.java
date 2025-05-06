1604721cf6495073ad54d36129406d702ef059f3
Delvaux-Jean-Baptiste <59822016+Delvaux-Jean-Baptiste@users.noreply.github.com>
2025-05-06 17:33:33 +0200

    Implemented Sapphire tutorial

--- src/main/java/net/elgate/arstoys/ArsToys.java
+++ src/main/java/net/elgate/arstoys/ArsToys.java
@@ -1,6 +1,9 @@
 package net.elgate.arstoys;
 
 import com.mojang.logging.LogUtils;
+import net.elgate.arstoys.item.ModCreativeModTabs;
+import net.elgate.arstoys.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +29,10 @@ public class ArsToys
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -45,7 +52,10 @@ public class ArsToys
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-//
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/elgate/arstoys/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.elgate.arstoys.item;
+
+import net.elgate.arstoys.ArsToys;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ArsToys.MODID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventbus){
+        ITEMS.register(eventbus);
+    }
+
+}
